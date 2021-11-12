52d59d462d60c90b669f46afafee663c3ae5d866
romuloLadeira <romulo.alesandro@gmail.com>
2021-11-12 18:19:23 -0300

    excluir
    adicionei  a funcionalidade de excluir itens

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,45 @@
 
 let novaTarefa = document.getElementById("novaTarefa") 
 let listaTarefas = document.getElementById("listaTarefas")
+
+
 btnTarefa.addEventListener('click',event => {
     let valorNtarefa = novaTarefa.value
+   
+    let li = document.createElement('li')
+    li.innerText = valorNtarefa
+    
+    let button = document.createElement('button')
+    button.innerText ='X'
+    button.className = "btnExcluirTarefa"
+    button.setAttribute("onclick", "excluir(this)")
 
-    listaTarefas.innerHTML += `<li><input type="checkbox" class="marcarFeito" onclick="marcar(this)">  ${valorNtarefa}  <button class="btnExcluirTarefa" onclick="excluir(this)">X</button></li>`
+    let checkbox = document.createElement('input') 
+    checkbox.className ="marcarFeito"
+    checkbox.setAttribute("onclick", "marcar(this)" )
+    checkbox.setAttribute("type", "checkbox")
+    
+    li.appendChild(button)
+    li.appendChild(checkbox)
+
+    listaTarefas.appendChild(li)
+    
 })
 
 function marcar(checkbox){
     
     if(checkbox.checked === true){
         checkbox.parentElement.style.textDecoration ="line-through"
+        
     }
     else{
         checkbox.parentElement.style.textDecoration ="none"
+        
     }
     
     }
 function excluir(btn){
+    btn.parentElement.remove()
     
 }
+
